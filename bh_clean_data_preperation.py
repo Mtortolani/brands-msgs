d0db0d9145f15e38cbd8ea224c5a45f7b8c44455
Marco Tortolani <80783579+Mtortolani@users.noreply.github.com>
2022-05-06 15:38:14 -0400

    updated bj_clean_data_prep

--- bh_clean_data_preperation.py
+++ bh_clean_data_preperation.py
@@ -13,48 +13,48 @@ from file_opener import ReaderTools
 # df_survey = df_survey.rename(columns={"psid": "user_id"})
 # df_survey.head()
 
+
+# df_bh_clean_survey = df_bh_clean.merge(df_survey, on='user_id', how='left')
+# df_bh_clean_survey.head()
+
 # %%
-df_bh_clean = pd.read_csv('clean_files/bh_clean.csv', nrows=100000)
-domains = df_bh_clean['domain']
-domains_dict = dict(pd.value_counts(domains))
-domain_values = []
-for i, j in domains_dict.items():
-    domain_values.append([i,j])
-print(domain_values)
+df_bh_clean = pd.read_csv('clean_files/bh_clean.csv', nrows=1000)
+df_bh_clean.head()
+
+# domains = df_bh_clean['domain']
+# domains_dict = dict(pd.value_counts(domains))
+# domain_values = []
+# for i, j in domains_dict.items():
+#     domain_values.append([i,j])
+# print(domain_values)
     
 # with open('bh_clean_domains.csv', 'w+', newline='') as file:
 #     write = csv.writer(file)
 #     write.writerows(domain_values)
 
-print(df_bh_clean['domain'].value_counts())
-pd.value_counts(df_bh_clean['domain']).plot(kind='bar', figsize= (100,100))
+df_bh_clean.head()
+#pd.value_counts(df_bh_clean['domain']).plot(kind='bar', figsize= (100,100))
 
+categorized_domains = []
+with open('similarweb_categories.json', "r") as file:
+    similarweb = json.load(file)
+    
+similarweb = list(similarweb.items())
+for group in similarweb:
+    try:
+        for category in list(group[1].items()):
+            for url in category[1]:
+                            categorized_domains.append([url, category[0]])
+
+    except:
+        for url in group[1]:
+            categorized_domains.append([url, group[0]])
+
+df_cat_domains = pd.DataFrame(categorized_domains)
+df_cat_domains.columns = ['domain', 'similarweb_category']
+df_cat_domains.head()
+
+df_bh_clean = df_bh_clean.merge(df_cat_domains, on='domain', how='left')
+df_bh_clean.head(100)
 # %%
-# df_bh_clean_survey = df_bh_clean.merge(df_survey, on='user_id', how='left')
-# df_bh_clean_survey.head()
-
-
-
-
-
-
-
-
-# Getting categories from google ad data
-# file = open('condensed_ad_preferences\google_ads_bz2_preferences_MT.json')
-# google_ads_preferences = json.load(file)
-
-# preferences_unique = []
-
-# for preferences in google_ads_preferences.values():
-#     print(len(set(preferences)))
-#     for x in set(preferences):
-#         preferences_unique.append(x)
-
-# file.close()
-
-
-
-# preferences_unique = list(set(preferences_unique))
-# print(len(preferences_unique))
-# print(preferences_unique)
+# list(x[1][1].items())[1]
